f432c9426e6a8a229f8e28891c0e7dc5167b09b2
licheng <licheng@cs.unc.edu>
2019-12-20 05:41:47 +0000

    scripts and configs for training faster r-cnn with attributes

--- experiments/genome_faster_rcnn_attr_R_50_RPN_1x.sh
+++ experiments/genome_faster_rcnn_attr_R_50_RPN_1x.sh
@@ -5,7 +5,7 @@ attr_sampling="expminus"
 # training
 python tools/train_net.py \
         --num-gpus 8 \
-        --config-file configs/GENOME-Detection/faster_rcnn_softmax_attr_R_50_FPN_1x.yaml \
+        --config-file configs/GENOME-Detection/faster_rcnn_${attr_loss_type}_attr_R_50_FPN_1x.yaml \
         "MODEL.ROI_HEADS.ATTRIBUTE_LOSS_TYPE" ${attr_loss_type} \
         "MODEL.ROI_HEADS.ATTRIBUTE_LOSS_WEIGHT" ${attr_loss_weight} \
         "MODEL.ROI_HEADS.ATTRIBUTE_SAMPLING" ${attr_sampling} \
@@ -14,14 +14,14 @@ python tools/train_net.py \
 # evaluation
 python tools/train_net.py \
         --num-gpus 8 \
-        --config-file configs/GENOME-Detection/faster_rcnn_softmax_attr_R_50_FPN_1x.yaml \
+        --config-file configs/GENOME-Detection/faster_rcnn_${attr_loss_type}_attr_R_50_FPN_1x.yaml \
         --eval-only \
         "MODEL.WEIGHTS" "output/genome_faster_rcnn_${attr_loss_type}${attr_loss_weight}_${attr_sampling}_attr_R_50_FPN_1x/model_final.pth"
 
 # attribute evaluation
 python tools/train_net.py \
         --num-gpus 8 \
-        --config-file configs/GENOME-Detection/faster_rcnn_softmax_attr_R_50_FPN_1x.yaml \
+        --config-file configs/GENOME-Detection/faster_rcnn_${attr_loss_type}_attr_R_50_FPN_1x.yaml \
         --eval-only \
         --eval-attribute \
         "MODEL.WEIGHTS" "output/genome_faster_rcnn_${attr_loss_type}${attr_loss_weight}_${attr_sampling}_attr_R_50_FPN_1x/model_final.pth"
