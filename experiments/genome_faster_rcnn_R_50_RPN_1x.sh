79eb018b7226c242a2a39037be04f7d02113beea
licheng <licheng@cs.unc.edu>
2019-12-20 05:44:05 +0000

    add evaluation script

--- experiments/genome_faster_rcnn_R_50_RPN_1x.sh
+++ experiments/genome_faster_rcnn_R_50_RPN_1x.sh
@@ -3,9 +3,9 @@ python tools/train_net.py \
         --num-gpus 8 \
         --config-file configs/GENOME-Detection/faster_rcnn_R_50_FPN_1x.yaml 
 
-# # evaluation
-# python tools/train_net.py \
-#         --num-gpus 8 \
-#         --config-file configs/GENOME-Detection/faster_rcnn_R_50_FPN_1x.yaml \
-#         --eval-only \
-#         'MODEL.WEIGHTS' 'output/genome_faster_rcnn_R_50_RPN_1x/model_final.pth' 
+# evaluation
+python tools/train_net.py \
+        --num-gpus 8 \
+        --config-file configs/GENOME-Detection/faster_rcnn_R_50_FPN_1x.yaml \
+        --eval-only \
+        'MODEL.WEIGHTS' 'output/genome_faster_rcnn_R_50_FPN_1x/model_final.pth' 
