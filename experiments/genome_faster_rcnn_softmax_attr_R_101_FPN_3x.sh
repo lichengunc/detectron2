e15c0e8cf1a1cb9bc266e053f2253b79db39d807
licheng <licheng@cs.unc.edu>
2020-01-07 00:14:50 +0000

    script training genome_faster_rcnn softmax_attr FPN 3x

---
+++ experiments/genome_faster_rcnn_softmax_attr_R_101_FPN_3x.sh
@@ -0,0 +1,28 @@
+attr_loss_weight=0.5
+attr_loss_type="softmax"
+attr_sampling="expminus"
+
+# training
+python tools/train_net.py \
+        --num-gpus 8 \
+        --config-file configs/GENOME-Detection/faster_rcnn_softmax_attr_R_101_FPN_3x.yaml \
+        "MODEL.ROI_HEADS.ATTRIBUTE_LOSS_TYPE" ${attr_loss_type} \
+        "MODEL.ROI_HEADS.ATTRIBUTE_LOSS_WEIGHT" ${attr_loss_weight} \
+        "MODEL.ROI_HEADS.ATTRIBUTE_SAMPLING" ${attr_sampling} \
+        "OUTPUT_DIR" "output/genome_faster_rcnn_${attr_loss_type}${attr_loss_weight}_${attr_sampling}_attr_R_101_FPN_3x"
+
+# evaluation
+python tools/train_net.py \
+        --num-gpus 8 \
+        --config-file configs/GENOME-Detection/faster_rcnn_softmax_attr_R_101_FPN_3x.yaml \
+        --eval-only \
+        "MODEL.WEIGHTS" "output/genome_faster_rcnn_${attr_loss_type}${attr_loss_weight}_${attr_sampling}_attr_R_101_FPN_3x/model_final.pth"
+
+# attribute evaluation
+python tools/train_net.py \
+        --num-gpus 8 \
+        --config-file configs/GENOME-Detection/faster_rcnn_softmax_attr_R_101_FPN_3x.yaml \
+        --eval-only \
+        --eval-attribute \
+        "MODEL.WEIGHTS" "output/genome_faster_rcnn_${attr_loss_type}${attr_loss_weight}_${attr_sampling}_attr_R_101_FPN_3x/model_final.pth"
+
